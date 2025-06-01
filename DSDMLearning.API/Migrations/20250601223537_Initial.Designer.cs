4c60b59ca90c26412f5d700e66785b37b7db02c9
Nicolas <nicolasrapp05@gmail.com>
2025-06-01 20:11:15 -0300

    Ajustes

--- DSDMLearning.API/Migrations/20250601223537_Initial.Designer.cs
+++ DSDMLearning.API/Migrations/20250601223537_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DSDMLearning.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250528184112_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20250601223537_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
